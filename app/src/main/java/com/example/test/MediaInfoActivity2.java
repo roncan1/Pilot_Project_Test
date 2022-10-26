ddb4d5df877bf5d9b49470e5e4e22ee1d262ec80
최지민 <jiminii4810@gmail.com>
2022-10-26 16:04:38 +0900

    권한체크 정상화

--- app/src/main/java/com/example/test/MediaInfoActivity2.java
+++ app/src/main/java/com/example/test/MediaInfoActivity2.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.AppOpsManager;
 import android.app.usage.StorageStats;
 import android.app.usage.StorageStatsManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
@@ -41,34 +43,35 @@ public class MediaInfoActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media_info2);
 
-        all = getIntent().getLongExtra("all", 0);
-
         // 앱 용량 확인에 필요한 권한 취득
         int a = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.PACKAGE_USAGE_STATS);
-        if (a == PackageManager.PERMISSION_DENIED) {
+        if (needPermissionForBlocking(getApplicationContext())) {
+
             Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
             startActivity(intent);
+        } else {
+            all = getIntent().getLongExtra("all", 0);
+
+            TextView tv_img = findViewById(R.id.tv_img2);
+            TextView tv_app = findViewById(R.id.tv_app2);
+            tv_img.setText(unitConversion(getAllImageSizeByByte()) + " / " + imgCount + "개");
+            tv_app.setText(unitConversion(test2()) + " / " + appCount + "개");
+
+            apps = test2();
+
+
+            // 퍼센트 확인 테스트
+            TextView tv_percent = findViewById(R.id.tv_percent);
+            Log.d("TAG", "onCreate: " + image);
+            Log.d("TAG", "onCreate: " + all);
+            int imagePercent = (int) ((double) image / (double) all * 100);
+            int appsPercent = (int) ((double) apps / (double) all * 100);
+            tv_percent.setText("이미지 : " + imagePercent + "% / " + "앱 : " + appsPercent + "%");
         }
 
-        TextView tv_img = findViewById(R.id.tv_img2);
-        TextView tv_app = findViewById(R.id.tv_app2);
-        tv_img.setText(unitConversion(getAllImageSizeByByte()) + " / " + imgCount + "개");
-        tv_app.setText(unitConversion(test2()) + " / " + appCount + "개");
-
-        apps = test2();
-
-
-        // 퍼센트 확인 테스트
-        TextView tv_percent = findViewById(R.id.tv_percent);
-        Log.d("TAG", "onCreate: " + image);
-        Log.d("TAG", "onCreate: " + all);
-        int imagePercent = (int) ((double) image / (double) all * 100);
-        int appsPercent = (int) ((double) apps / (double) all * 100);
-        tv_percent.setText("이미지 : " + imagePercent + "% / " + "앱 : " + appsPercent + "%");
-
-
-
     }
+
+
 
     private long getAllImageSizeByByte() {
         // 어플 용량
@@ -79,10 +82,9 @@ public class MediaInfoActivity2 extends AppCompatActivity {
 
         // MediaStore를 이용해서 무슨 정보를 받아올 것인가
         String[] projection = new String[]{
-                MediaStore.Images.Media._ID,
-                MediaStore.Images.Media.DISPLAY_NAME,
-                MediaStore.Images.Media.MIME_TYPE,
-                MediaStore.Images.Media.SIZE,
+                MediaStore.Images.Media.DISPLAY_NAME, // 파일 이름
+                MediaStore.Images.Media.DATE_MODIFIED, // 수정된 날짜
+                MediaStore.Images.Media.SIZE, // 파일 크기
         };
 
         // 파일목록 쿼리 (3,4,5번 인자값을 null로 설정해 모든 이미지파일을 받아옴)
@@ -91,15 +93,19 @@ public class MediaInfoActivity2 extends AppCompatActivity {
                 projection, // 어떤 정보?
                 null, // 어떤 조건으로? (where 절)
                 null, //``
-                null// 무엇을 기준으로 정렬?
+                null  // 무엇을 기준으로 정렬?
         );
 
         // 원하는 데이터가 저장된 column값을 받아옴
         int sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.SIZE);
+        int nameColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DISPLAY_NAME);
+        int dateColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.Media.DATE_MODIFIED);
 
         // 모든 이미지 파일을 하나씩 탐색하며 원하는 동작을 처리
         while (cursor.moveToNext()) {
             int size = cursor.getInt(sizeColumn);
+            String name = cursor.getString(nameColumn);
+            String date = cursor.getString(dateColumn);
             bytes += size;
             imgCount++;
         }
@@ -127,6 +133,18 @@ public class MediaInfoActivity2 extends AppCompatActivity {
 //
 //
 //
+
+    public static boolean needPermissionForBlocking(Context context) {
+        try {
+            PackageManager packageManager = context.getPackageManager();
+            ApplicationInfo applicationInfo = packageManager.getApplicationInfo(context.getPackageName(), 0);
+            AppOpsManager appOpsManager = (AppOpsManager) context.getSystemService(Context.APP_OPS_SERVICE);
+            int mode = appOpsManager.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS, applicationInfo.uid, applicationInfo.packageName);
+            return  (mode != AppOpsManager.MODE_ALLOWED);
+        } catch (PackageManager.NameNotFoundException e) {
+            return true;
+        }
+    }
 
     public long test2() {
         // 패키지 매니저 받아오기
